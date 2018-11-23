c1079092e5d8d66b094387fbb3f9d2d8d0a5f43a
Toni <toni.turpeinen@helsinki.fi>
2018-11-23 23:24:55 +0200

    Added examples to NewMain

--- MatrixCalc/src/main/java/matrixcalc/domain/NewMain.java
+++ MatrixCalc/src/main/java/matrixcalc/domain/NewMain.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package matrixcalc.domain;
 
+import matrixcalc.io.MatrixPrinter;
+import matrixcalc.test.GenerateMatrix;
 import matrixcalc.test.Test;
 
 /**
@@ -18,17 +16,60 @@ public class NewMain {
      */
     public static void main(String[] args) {
         
-        //testataan kakkosen potensseilla:
-        //int pow2 = 1;
+        //SUGGESTIONS FOR WHAT TO DO:
         
+        //Run Determinant Tests
         int runsPerTest = 10;
         int size = 0;
-        
-        for(int i = 0; i<11; i++){
+        //Add iterations for larger tests
+        System.out.println("DETERMINANT SPEED TESTS: ");
+        for(int i = 0; i<6; i++){
             Test.testDeterminantSpeed(size, runsPerTest);
             size +=100;
         }
+        System.out.println("");
         
+        
+        //Run Multiplication Tests
+        int runs = 10;
+        int powerOfTwo = 1;
+        //Add iterations for larger tests
+        System.out.println("MULTIPLICATION SPEED TESTS:");
+        for(int i = 0; i<6; i++){
+            Test.testMultiplicationSpeed(powerOfTwo, runs);
+            powerOfTwo *= 2;
+        }
+        System.out.println("");
+        
+        
+        //Add Matrices And Print
+        double[][] A = GenerateMatrix.generate(4, -100, 100);
+        double[][] B = GenerateMatrix.generate(4, -100, 100);
+        System.out.println("");
+        System.out.println("ADDED MATRIX: ");
+        System.out.println("");
+        double[][] Added = Addition.add(A, B);
+        MatrixPrinter.print(Added);
+        
+        
+        //Multiply Matrices And Print
+        double[][] multA = GenerateMatrix.generate(4, -100, 100);
+        double[][] multB = GenerateMatrix.generate(4, -100, 100);
+        System.out.println("");
+        System.out.println("Multiplied Matrix: ");
+        System.out.println("");
+        double[][] Multiplied = Multiplication.multiply(A, B);
+        MatrixPrinter.print(Multiplied);
+        
+        
+        //Get Determinant And Print
+        double[][] X = GenerateMatrix.generate(4, -100, 100);
+        System.out.println("");
+        System.out.println("Determinant: ");
+        System.out.println("");
+        double determinant = Determinant.determinant(X);
+        System.out.println(determinant);
+    
     }
     
 }

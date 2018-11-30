1caa3a1451ff8cc0da998980fb19fec6be3769fd
Toni <toni.turpeinen@helsinki.fi>
2018-11-30 23:43:30 +0200

    Started GUI
    ParseA should work with square matrices with "," delimiter

---
+++ MatrixCalc/src/main/java/matrixcalc/domain/Main.java
@@ -0,0 +1,19 @@
+
+package matrixcalc.domain;
+
+import matrixcalc.ui.GUI;
+
+/**
+ *
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        GUI.main(args);
+    }
+}
--- MatrixCalc/src/main/java/matrixcalc/io/MatrixScanner.java
+++ MatrixCalc/src/main/java/matrixcalc/io/MatrixScanner.java
@@ -49,4 +49,24 @@ public class MatrixScanner {
         
         return a;
     }
+    
+    public static double[][] buildMatrixFromGUI(String input){
+        String[] numbers = input.split(",");
+        for(String number : numbers){
+            System.out.println(number);
+        }
+        int size = (int) Math.sqrt(numbers.length);
+        double[][] a = new double[size][size];
+        int i = 0;
+        int j = 0;
+        for(String number : numbers){
+            a[j][i] = Double.valueOf(number);
+            i++;
+            if(i==size){
+                i=0;
+                j++;
+            }
+        }
+        return a;
+    }
 }

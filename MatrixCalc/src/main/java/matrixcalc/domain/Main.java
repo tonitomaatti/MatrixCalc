4fbe114447104d669df181284ea631c1b2f091a4
Toni <toni.turpeinen@helsinki.fi>
2018-11-30 23:49:58 +0200

    some explanations

--- MatrixCalc/src/main/java/matrixcalc/domain/Main.java
+++ MatrixCalc/src/main/java/matrixcalc/domain/Main.java
@@ -13,7 +13,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        //Starts GUI for now. If you want to run tests copy code from matrixcacltodo.txt
+        //GUI parse A -button should work with MatrixA text field with square matrix. Write "1,2,3,4", for example.
         GUI.main(args);
     }
 }
